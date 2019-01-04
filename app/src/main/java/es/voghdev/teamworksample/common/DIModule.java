79911d6a339dc31308dcef2cc97ce315e5685d39
Olmo Gallegos <vendofiatpunto@hotmail.com>
2019-01-04 20:22:57 +0100

    Refactor to use ApiDataSource again instead of mocks

--- app/src/main/java/es/voghdev/teamworksample/common/DIModule.java
+++ app/src/main/java/es/voghdev/teamworksample/common/DIModule.java
@@ -1,14 +1,15 @@
 package es.voghdev.teamworksample.common;
 
+import es.voghdev.teamworksample.BuildConfig;
 import es.voghdev.teamworksample.features.projects.ProjectRepository;
-import es.voghdev.teamworksample.features.projects.mock.GetProjectsMockDataSource;
+import es.voghdev.teamworksample.features.projects.api.GetProjectsApiDataSource;
 
 public class DIModule {
     static ProjectRepository projectRepository;
 
     public static ProjectRepository getProjectRepository() {
         if (null == projectRepository)
-            projectRepository = new ProjectRepository(new GetProjectsMockDataSource()); // new GetProjectsApiDataSource(BuildConfig.TeamWorkApiToken)
+            projectRepository = new ProjectRepository(new GetProjectsApiDataSource(BuildConfig.TeamWorkApiToken));
 
         return projectRepository;
     }
--- app/src/main/java/es/voghdev/teamworksample/features/projects/api/AuthInterceptor.kt
+++ app/src/main/java/es/voghdev/teamworksample/features/projects/api/AuthInterceptor.kt
@@ -1,12 +1,14 @@
 package es.voghdev.teamworksample.features.projects.api
 
+import android.util.Base64
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 
 class AuthInterceptor(val apiToken: String) : Interceptor {
     override fun intercept(chain: Interceptor.Chain?): Response {
-        val request = chain?.request()?.newBuilder()?.addHeader("Authorization", apiToken)
+        val authorizationHeader = Base64.encodeToString((apiToken).toByteArray(), Base64.NO_WRAP)
+        val request = chain?.request()?.newBuilder()?.addHeader("Authorization", "Basic $authorizationHeader")
                 ?.addHeader("Accept", "application/json")
                 ?.addHeader("Content-Type", "application/json")?.build()
                 ?: Request.Builder().build()
